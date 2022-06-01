dd1cc4adcf559cfe4e722f95a2ff7ff67c7e2bf5
natsoleary <nmoleary@usc.edu>
2022-06-01 00:47:49 -0400

    Unity files containing project and code that very briefly creates a new triangle and drops it into the scene

---
+++ Stack2019/Assets/Scripts/ChoosePile.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChoosePile : MonoBehaviour
+{
+    [SerializeField] private GameObject one;
+    [SerializeField] private GameObject two;
+    [SerializeField] private GameObject three;
+    [SerializeField] private GameObject four;
+    [SerializeField] private GameObject five;
+    [SerializeField] private GameObject six;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject stackbase = GameObject.FindWithTag("1");
+        Vector3 base_pos = stackbase.transform.position;
+        Vector3 spawn_pos = base_pos;
+        spawn_pos.y += 20; 
+        Instantiate(one, spawn_pos, stackbase.transform.rotation);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // GetVal();
+    }
+
+}
