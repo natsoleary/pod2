89b9e4bd0551f9c1b8351fecfc967b677cb409b8
natsoleary <nmoleary@usc.edu>
2022-06-01 01:04:43 -0400

    walkthrough

--- Stack2019/Assets/Scripts/ChoosePile.cs
+++ Stack2019/Assets/Scripts/ChoosePile.cs
@@ -4,13 +4,7 @@ using UnityEngine;
 
 public class ChoosePile : MonoBehaviour
 {
-    [SerializeField] private GameObject one;
-    [SerializeField] private GameObject two;
-    [SerializeField] private GameObject three;
-    [SerializeField] private GameObject four;
-    [SerializeField] private GameObject five;
-    [SerializeField] private GameObject six;
-
+    [SerializeField] private GameObject slice;
 
     // Start is called before the first frame update
     void Start()
@@ -19,7 +13,7 @@ public class ChoosePile : MonoBehaviour
         Vector3 base_pos = stackbase.transform.position;
         Vector3 spawn_pos = base_pos;
         spawn_pos.y += 20; 
-        Instantiate(one, spawn_pos, stackbase.transform.rotation);
+        Instantiate(slice, spawn_pos, stackbase.transform.rotation);
 
     }
 
